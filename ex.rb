2a4b6d00024e482ec28e69f96fed734aaa995291
rdp <rogerpack2005@gmail.com>
2009-10-28 16:57:24 -0600

    wow it works

--- ex.rb
+++ ex.rb
@@ -1,12 +1,11 @@
 require 'frubygems'
 require 'sane'
 require 'ruby-wmi'
-
 #all = WMI::Win32_PerfFormattedData_PerfProc_Process.find(:all)
 #all.map &:Refresh_
 #all.map &:Refresh_ # for some reason have to run it twice
 #puts all.map &:PercentProcessorTime # will be the real values
-
+require 'benchmark'
 # this is snapshot of raw data per process
 # including 
 WMI::Win32_PerfRawData_PerfProc_Process.find(:all)[0].PercentProcessorTime
@@ -14,31 +13,40 @@ WMI::Win32_PerfRawData_PerfProc_Process.find(:all)[0].PercentProcessorTime
 
 
 def fill into_this
-  a = nil
-  WMI::Win32_PerfRawData_PerfProc_Process.find(:all).each{|proc|
+  all = WMI::Win32_PerfRawData_PerfProc_Process.find(:all).each{|proc|
     into_this[proc.IDProcess] = proc.PercentProcessorTime.to_i
-    a = proc.TimeStamp_Sys100NS
   }
-  a
+  all[0].TimeStamp_Sys100NS.to_i # these are all the same
 end
 
 # anticipate {123 => 456}
 
-def diff_values first, second
+def diff_values first, second, time_diff
+  puts time_diff, 'was time diff'
   answer = {}
   for key in first.keys
     if first[key] != second[key]
-      answer[key] = first[key] - second[key]
+      answer[key] = (first[key] - second[key]).to_f/time_diff*100
     end
   end
   answer
 end
 
 old = {}
+
 ts1 = fill old
 sleep 0.1
 new = {}
 ts2 = fill new
-puts (diff_values new, old).inspect
-puts new.sort.inspect
-puts ts1, ts2
+
+sorted = diff_values( new, old, ts2 - ts1 )
+puts sorted.inspect
+violated = []
+sorted.each{|pid, percentage|
+ if percentage == 100 # using a full core
+   violated << pid
+   proc =WMI::Win32_Process.find(:first,  :conditions => {:ProcessId => pid.to_i})
+   proc.SetPriority 64 # low priority
+ end
+}
+puts violated.inspect
