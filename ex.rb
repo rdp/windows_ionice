5c002d44d780c92ae34469ee4f1a28670f398cb2
rdp <rogerpack2005@gmail.com>
2009-11-18 15:06:54 -0700

    also add a penalty if a process reads > 1 MB/s of data

--- ex.rb
+++ ex.rb
@@ -14,19 +14,25 @@ WMI::Win32_PerfRawData_PerfProc_Process.find(:all)[0].PercentProcessorTime
 
 def fill into_this
   all = WMI::Win32_PerfRawData_PerfProc_Process.find(:all).each{|proc|
-    into_this[proc.IDProcess] = [proc.PercentProcessorTime.to_i, proc.IODataBytesPerSec.to_i]
+    into_this[proc.IDProcess] = {:processor => proc.PercentProcessorTime.to_i, :data => proc.IODataBytesPerSec.to_i}
   }
   return all[0].TimeStamp_Sys100NS.to_i # these are all the same
 end
 
 # anticipate {123 => 456}
 
-def diff_values first, second, time_diff
+def diff_values first, second, time_diff, what_to_calculate
   answer = {}
   for key in first.keys
     if first[key] != second[key] # sometimes they stay the same
       begin
-        answer[key] = (first[key][0] - second[key][0]).to_f/time_diff*100
+        answer[key] = (first[key][what_to_calculate] - second[key][what_to_calculate]).to_f/time_diff
+        if what_to_calculate == :data
+            answer[key] *= 10_000_000 # strip off the 100_ns aspect
+        else
+            answer[key] *= 100 # percentages come as 0.99 -- we want 99 to mean 99% of one core
+        end
+
       rescue
         # sometimes the values are nil
         # ignore I suppose
@@ -42,21 +48,31 @@ loop {
   old = {}
 
   ts1 = fill old
-  sleep 3
+  $VERBOSE ? sleep( 1 ) : sleep ( 3 )
   new = {}
   ts2 = fill new
 
-  sorted = diff_values( new, old, ts2 - ts1 )
-  puts sorted.inspect if $VERBOSE
-  violated = []
-  sorted.each{|pid, percentage|
-    if percentage >= 98 && pid != 0 # using a full core (100) or more...
-      violated << pid
-      proc = WMI::Win32_Process.find(:first,  :conditions => {:ProcessId => pid.to_i})
-      if proc.Priority > 4 # appears that 7 or 8 here mean normal prio...
-        proc.SetPriority 64 # set it to low priority
-        puts "violated!", pid
+  for value in [:processor, :data]
+    sorted = diff_values( new, old, ts2 - ts1, value ) # give use "just data" or not
+    puts value, sorted.inspect if $VERBOSE
+    violated = []
+
+    sorted.each{|pid, reading|
+
+      if value == :processor
+        max = 97 # > 97% of one core -> penalt!
+      elsif value == :data
+        max = 1_000_000 # more than 1 MB/s -> penalt!
       end
-    end
-  }
+
+      if reading > max && pid != 0 # using a full core (100) or more...
+        violated << pid
+        proc = WMI::Win32_Process.find(:first,  :conditions => {:ProcessId => pid.to_i})
+        if proc.Priority > 4 # appears that 7 or 8 here mean normal prio...
+          proc.SetPriority 64 # set it to low priority
+          puts "violated!", pid, value, reading
+        end
+      end
+    }
+  end
 }
